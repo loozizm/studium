5121b1ce17f7758648f72b96ce1e864be312cb8d
Shota Maekawa <f22raptor0817@gmail.com>
2019-08-16 06:37:02 +0900

    eslint設定を変更

--- hosting/.eslintrc.js
+++ hosting/.eslintrc.js
@@ -1,15 +1,22 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    browser: true,
+    es6: true,
   },
   'extends': [
     'plugin:vue/essential',
-    'eslint:recommended'
+    'eslint:recommended',
+    "google",
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "quotes": ["error", "double"],
+    "space-before-blocks": "off",
+    "arrow-parens": ["error", "as-needed"],
+    "require-jsdoc": "off",
   },
   parserOptions: {
     parser: 'babel-eslint'
