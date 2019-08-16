af8130498be53aa9a48a3d17a02c0311a3468fe8
Shota Maekawa <f22raptor0817@gmail.com>
2019-08-16 10:14:55 +0900

    ヘッダのサンプルを追加

--- hosting/src/App.vue
+++ hosting/src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <v-app>
     <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title class="headline">
+        <span>TITLE</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-btn text v-if="!auth">SIGN IN</v-btn>
+      <v-btn text v-if="!auth">SIGN UP</v-btn>
+      <v-btn text v-if="auth">SIGN OUT</v-btn>
     </v-app-bar>
 
     <v-content>
@@ -32,5 +28,10 @@ export default {
   data: () => ({
     //
   }),
+  computed: {
+    auth(){
+      return this.$store.state.auth;
+    },
+  },
 };
 </script>
