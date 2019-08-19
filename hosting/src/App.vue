b187dc5b43cdcbc2d6a75f43fd9afeeaef870a60
Shota Maekawa <f22raptor0817@gmail.com>
2019-08-19 21:21:16 +0900

    サイドバーのサンプルを追加

--- hosting/src/App.vue
+++ hosting/src/App.vue
@@ -1,7 +1,29 @@
 <template>
   <v-app>
+    <v-navigation-drawer
+      app
+      v-model="drawer"
+      bottom
+      temporary
+    >
+      <v-list rounded>
+        <v-list-item-group v-model="item" color="primary">
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+          >
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
     <v-app-bar app>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="headline">
         <span>TITLE</span>
       </v-toolbar-title>
@@ -26,7 +48,13 @@ export default {
     HelloWorld,
   },
   data: () => ({
-    //
+    drawer: null,
+    item: 1,
+    items: [
+      {text: "Real-Time", icon: "mdi-clock"},
+      {text: "Audience", icon: "mdi-account"},
+      {text: "Conversions", icon: "mdi-flag"},
+    ],
   }),
   computed: {
     auth(){
